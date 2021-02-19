6dbd18182b509fc6ad945ffee05b13b698f11243
chudaraj poudyal <crpoudyal@gmail.com>
2021-02-19 21:17:21 +0545

    css fix

--- src/components/Pages/Products/products.component.js
+++ src/components/Pages/Products/products.component.js
@@ -7,18 +7,18 @@ export const Products = () => {
     return (
         <>
             <NavBar isLoggedIn={false}></NavBar>
-            <div className="small-container single-product">
+            <div className="Psmall-container single-product">
                 <div className="head">
                     <div className="col-2 image">
                         <img src="../images/cactus.jpg" height="500" />
                     </div>
 
-                    <div className="col-2 details">
+                    <div className="col-2 Pdetails">
                         <p>Indoor Plant</p>
                         <h1>Cactus</h1>
                         <h4>Rs 1200</h4>
                         {/* <input type="number" value="1" /> */}
-                        <button><a href="">Add to Cart </a></button>
+                        <button>Add to Cart</button>
                         <h3>product Detail <i className="fas fa-info-circle"></i></h3>
                         <p>
                             A cactus is a member of the plant family Cactaceae, a family
@@ -32,7 +32,7 @@ export const Products = () => {
 
 
 
-                <div className="small-container">
+                <div className="Psmall-container">
                     <div className="product-list">
                         <h2>Related Products</h2>
                         <div className="row grid">
--- src/components/Section/category/category.component.js
+++ src/components/Section/category/category.component.js
@@ -5,68 +5,68 @@ export const Category = () => {
     return (
         <>
             <h2 className="title">Categories</h2>
-            <div className="categories">
+            <div className="Categories">
                 <div className="grid-view">
-                    <div className="image">
+                    <div className="Cimage">
                         <h1>Name</h1>
                         <img src="../images/cactus.jpg" alt="" />
-                        <div className="overlay">
-                            <div className="overlay-content">
+                        <div className="Coverlay">
+                            <div className="Coverlay-content">
                                 <h2>Indoor plants <i className="fas fa-info-circle"></i></h2>
                                 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, eligendi? Cupiditate eum delectus accusamus quis.</p>
                                 <a href="#"> <button>view more</button></a>
                             </div>
                         </div>
                     </div>
-                    <div className="image">
+                    <div className="Cimage">
                         <h1>Name</h1>
                         <img src="../images/cactus.jpg" alt="" />
-                        <div className="overlay">
-                            <div className="overlay-content">
+                        <div className="Coverlay">
+                            <div className="Coverlay-content">
                                 <h2>Indoor plants <i className="fas fa-info-circle"></i></h2>
                                 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, eligendi? Cupiditate eum delectus accusamus quis.</p>
                                 <a href="#"> <button>view more</button></a>
                             </div>
                         </div>
                     </div>
-                    <div className="image">
+                    <div className="Cimage">
                         <h1>Name</h1>
                         <img src="../images/plant.jpg" alt="" />
-                        <div className="overlay">
-                            <div className="overlay-content">
+                        <div className="Coverlay">
+                            <div className="Coverlay-content">
                                 <h2>Indoor plants <i className="fas fa-info-circle"></i></h2>
                                 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, eligendi? Cupiditate eum delectus accusamus quis.</p>
                                 <a href="#"> <button>view more</button></a>
                             </div>
                         </div>
                     </div>
-                    <div className="image">
+                    <div className="Cimage">
                         <h1>Name</h1>
                         <img src="../images/plant1.jpg" alt="" />
-                        <div className="overlay">
-                            <div className="overlay-content">
+                        <div className="Coverlay">
+                            <div className="Coverlay-content">
                                 <h2>Indoor plants <i className="fas fa-info-circle"></i></h2>
                                 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, eligendi? Cupiditate eum delectus accusamus quis.</p>
                                 <a href="#"> <button>view more</button></a>
                             </div>
                         </div>
                     </div>
-                    <div className="image">
+                    <div className="Cimage">
                         <h1>Name</h1>
                         <img src="../images/plant2.jpg" alt="" />
-                        <div className="overlay">
-                            <div className="overlay-content">
+                        <div className="Coverlay">
+                            <div className="Coverlay-content">
                                 <h2>kitchen Gardening <i className="fas fa-info-circle"></i></h2>
                                 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, eligendi? Cupiditate eum delectus accusamus quis.</p>
                                 <a href="#"> <button>view more</button></a>
                             </div>
                         </div>
                     </div>
-                    <div className="image">
+                    <div className="Cimage">
                         <h1>Name</h1>
                         <img src="../images/plant.jpg" alt="" />
-                        <div className="overlay">
-                            <div className="overlay-content">
+                        <div className="Coverlay">
+                            <div className="Coverlay-content">
                                 <h2>Outdoor plants <i className="fas fa-info-circle"></i></h2>
                                 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, eligendi? Cupiditate eum delectus accusamus quis.</p>
                                 <a href="#"> <button>view more</button></a>
