b3c1ce68d1a0a4b7c18f7cbebf37ad0afa542616
ajitaz <azalbert.ab@gmail.com>
2021-02-24 14:55:04 +0545

    Dynamic User view

--- src/components/Dashboard/User/user.component.js
+++ src/components/Dashboard/User/user.component.js
@@ -1,86 +1,79 @@
-import React from 'react';
+import axios from 'axios';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Sidebar } from '../Sidebar/sidebar.component';
 import './user.component.css';
+const BASE_URL = 'http://localhost:80/plant_hugger_php';
 
-export const User = () => {
-    return (
-        <>
-            <Sidebar />
-            <div className="main-content">
-                <header>
-                    <div className="welcome">
-                        <p>Welcome, Ramesh</p>
-                    </div>
-                    <div className="social-icons">
-                    <Link to="/login"><button className="lbtn">Logout</button></Link>
-                    <Link to="/"><button className="sbtn">Home</button></Link>
-                    </div>
-                </header>
-                <main className="userMain">
-                    <h2 className="dash-title">Users-List</h2>
-                    <Link to="/dashboard/user/adduser"><button className="primary">Add user</button></Link>
-                    <div className="list">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Username</th>
-                                    <th>Email</th>
-                                    <th>Designation</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Admin</td>
-                                    <td>admin@gmail.com</td>
-                                    <td>Admin</td>
-                                    <td>
-                                        <i className="fas fa-pencil-alt"> Edit</i>
-                                        <i className="fas fa-trash-alt"> Delete</i>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>ramkrishna</td>
-                                    <td>Ramkrishna@gmail.com</td>
-                                    <td>user</td>
-                                    <td>
-                                        <i className="fas fa-pencil-alt"> Edit</i>
-                                        <i className="fas fa-trash-alt"> Delete</i>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Danish</td>
-                                    <td>danish@gmail.com</td>
-                                    <td>user</td>
-                                    <td>
-                                        <i className="fas fa-pencil-alt"> Edit</i>
-                                        <i className="fas fa-trash-alt"> Delete</i>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Nishant</td>
-                                    <td>Nishant@gmail.com</td>
-                                    <td>Nursery Admin</td>
-                                    <td>
-                                        <i className="fas fa-pencil-alt"> Edit</i>
-                                        <i className="fas fa-trash-alt"> Delete</i>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Utsav</td>
-                                    <td>utsav@gmail.com</td>
-                                    <td>Admin</td>
-                                    <td>
-                                        <i className="fas fa-pencil-alt"> Edit</i>
-                                        <i className="fas fa-trash-alt"> Delete</i>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </main>
-            </div>
-        </>
-    )
+export class User extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get(`${BASE_URL}/viewUser.php`)
+            .then(res => {
+                this.setState({
+                    data: res.data
+                })
+                console.log('Data in  State :', this.state);
+            })
+    }
+
+    render() {
+
+        return (
+            <>
+                <Sidebar />
+                <div className="main-content">
+                    <header>
+                        <div className="welcome">
+                            <p>Welcome, Ramesh</p>
+                        </div>
+                        <div className="social-icons">
+                            <Link to="/login"><button className="lbtn">Logout</button></Link>
+                            <Link to="/"><button className="sbtn">Home</button></Link>
+                        </div>
+                    </header>
+                    <main className="userMain">
+                        <h2 className="dash-title">Users-List</h2>
+                        <Link to="/dashboard/user/adduser"><button className="primary">Add user</button></Link>
+                        <div className="list">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Username</th>
+                                        <th>Email</th>
+                                        <th>Designation</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.data.map((result ,index) => {
+                                            return (
+                                                <tr key={index}>
+                                                    <td>{result.username}</td>
+                                                    <td>{result.email}</td>
+                                                    <td>{result.flag}</td>
+                                                    <td>
+                                                        <i className="fas fa-pencil-alt"> Edit</i>
+                                                        <i className="fas fa-trash-alt"> Delete</i>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </main>
+                </div>
+            </>
+        )
+    }
 }
