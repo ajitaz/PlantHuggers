1d3d55b716bbf7a967c6b1e2cab7a8d2684c116c
ajitaz <azalbert.ab@gmail.com>
2021-02-24 20:09:30 +0545

    minor change

--- src/components/Dashboard/Board/board.component.js
+++ src/components/Dashboard/Board/board.component.js
@@ -7,22 +7,21 @@ import './board.component.css';
 export class Dashboard extends Component {
     constructor() {
         super();
-        }
-    
+    }
+
 
     render() {
-      
         return (
             <>
                 <Sidebar />
                 <div className="main-content">
                     <header>
                         <div className="welcome">
-                            <p>Welcome, Ramesh</p>
+                            <p>Welcome</p>
                         </div>
                         <div className="social-icons">
-                        <Link to="/login"><button className="lbtn">Logout</button></Link>
-                        <Link to="/"><button className="sbtn">Home</button></Link>
+                            <Link to="/login"><button className="lbtn">Logout</button></Link>
+                            <Link to="/"><button className="sbtn">Home</button></Link>
                         </div>
                     </header>
                     <Overview />
--- src/components/Dashboard/Sidebar/sidebar.component.js
+++ src/components/Dashboard/Sidebar/sidebar.component.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Dashboard } from '../Board/board.component';
 import './sidebar.component.css';
 
 export class Sidebar extends Component {
