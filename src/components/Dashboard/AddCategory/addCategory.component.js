8d0709827abff5b1aaa2896d89b458b72135b8dc
ajitaz <azalbert.ab@gmail.com>
2021-02-28 15:54:12 +0545

    CategoryImage upload

--- src/components/Dashboard/AddCategory/addCategory.component.js
+++ src/components/Dashboard/AddCategory/addCategory.component.js
@@ -1,43 +1,88 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Sidebar } from '../Sidebar/sidebar.component';
 import './addCategory.component.css';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:80/plant_hugger_php/addCategory.php';
+
+export class AddCategory extends Component {
+    constructor() {
+        super();
+        this.state = {
+            cname: '',
+            description: '',
+            image: ''
+        }
+    }
+
+    handleChange = (e) => {
+        const { name, value, type } = e.target;
+        if (type === 'file') {
+            this.setState({
+                [name]: e.target.files[0]
+            })
+        } else {
+            this.setState({
+                [name]: value
+            })
+        }
+    }
 
 
-export const addCategory = () => {
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('cname', this.state.cname)
+        formData.append('description', this.state.description)
+        formData.append('image', this.state.image)
 
-    return (
-        <>
-            <Sidebar />
-            <div className="main-content">
-                <header>
-                    <div className="welcome">
-                        <p>Welcome, Ramesh</p>
-                    </div>
-                    <div className="social-icons">
-                        <Link to="/login"><button className="lbtn">Logout</button></Link>
-                        <Link to="/"><button className="sbtn">Home</button></Link>
-                    </div>
-                </header>
-                <main>
-                    <h2 className="dash-title">Add Category</h2>
+        axios.post(BASE_URL, formData, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Type': 'multipart/form-data'
+                
+            }, params: {},
+            responseType: 'json'
+        })
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    }
 
-                    <div className="form">
-                        <form method="POST" noValidate encType="multipart/form-data">
-                            <label htmlFor="cname">Category Name</label>
-                            <input type="text" id="cname" name="cname" placeholder="Category Name.." />
-                            <label htmlFor="desc">Description</label>
-                            <textarea type="text" id="description" name="description"/>
-                            <label htmlFor="img">Select image:</label>
-                            <input type="file" id="img" name="img"/>
-                        </form>
-                    </div>
-                </main>
+    render() {
+        return (
+            <>
+                <Sidebar />
+                <div className="main-content">
+                    <header>
+                        <div className="welcome">
+                            <p>Welcome, Ramesh</p>
+                        </div>
+                        <div className="social-icons">
+                            <Link to="/login"><button className="lbtn">Logout</button></Link>
+                            <Link to="/"><button className="sbtn">Home</button></Link>
+                        </div>
+                    </header>
+                    <main>
+                        <h2 className="dash-title">Add Category</h2>
 
-            </div>
-        </>
-    )
+                        <div className="form">
+                            <form onSubmit={this.handleSubmit} noValidate>
+                                <label htmlFor="cname">Category Name</label>
+                                <input type="text" name="cname" placeholder="Category Name.." onChange={this.handleChange} />
+                                <label htmlFor="desc">Description</label>
+                                <textarea type="text" name="description" onChange={this.handleChange} />
+                                <label htmlFor="img">Select image:</label>
+                                <input type="file" name="image" onChange={this.handleChange} />
+                                <input type="submit" value="ADD" />
+                            </form>
+                        </div>
+                    </main>
+
+                </div>
+            </>
+        )
+    }
+
 }
-       
-    
 
--- src/components/app.routing.js
+++ src/components/app.routing.js
@@ -16,7 +16,7 @@ import { AddProduct } from './Dashboard/AddProduct/addProduct.component';
 import { IndNursery } from './Pages/Ind_Nursery/indNursery.component';
 import { IndCategory } from './Pages/IndCategory/indCategory.component';
 import { setting } from './Dashboard/Setting/settng.component';
-import { addCategory } from './Dashboard/AddCategory/addCategory.component';
+import { AddCategory } from './Dashboard/AddCategory/addCategory.component';
 
 
 const NotFound = () => {
@@ -47,7 +47,7 @@ export const AppRouting = (props) => {
                 <Route exact path="/dashboard/addNursery" component={addNursery}></Route>
                 <Route exact path="/dashboard/viewNursery" component={viewNursery}></Route>
                 <Route exact path="/dashboard/setting" component ={setting}></Route>
-                <Route exact path="/dashboard/AddCategory" component ={addCategory}></Route>
+                <Route exact path="/dashboard/AddCategory" component ={AddCategory}></Route>
 
                 <Route component={NotFound}></Route>
             </Switch>
