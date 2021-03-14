e954832bd08380ef40f407f7026f157b0b430379
ajitaz <azalbert.ab@gmail.com>
2021-03-14 22:29:32 +0545

    Prodcts are fetched from database

--- src/components/Dashboard/ViewProduct/viewProduct.component.js
+++ src/components/Dashboard/ViewProduct/viewProduct.component.js
@@ -1,47 +1,83 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Sidebar } from '../Sidebar/sidebar.component';
 import { Link } from 'react-router-dom';
-import {DashNav} from '../DashNav/dashNav.component';
+import { DashNav } from '../DashNav/dashNav.component';
 import './viewProduct.component.css';
+import axios from 'axios';
 
-export const viewProduct = ()=>{
-    return(
-        <>
-          <Sidebar />
-         <div className="main-content">
-        <DashNav />
-         <main>
-            <h2 className="dash-title">Product-List</h2>
-           <Link to ="/dashboard/addProduct"><button className="primary">Add Product</button></Link>
-            <div className="list">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Product Name</th>
-                            <th>Product Image</th>
-                            <th>Categories</th>
-                            <th>tags</th>
-                            <th>Status</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>Catcus</td>
-                        <td><img src="../images/plant.jpg" alt="cactus"/></td>
-                        <td>Indoor</td>
-                        <td>cactus,indoor</td>
-                        <td><span className=" badge success">Avialiable</span></td>
-                        <td>
-                        <button className="edit"><i className="fas fa-pencil-alt"> Edit</i></button>
-                        <button className="delete"><i className="fas fa-trash-alt"> Delete</i></button>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-          </main>
-        </div>
-        </>
-    )
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+export class viewProduct extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            data: []
+        }
+
+    }
+
+    getProducts() {
+        axios.get(`${BASE_URL}/viewContent.php?option=viewProduct`)
+            .then(res => {
+                this.setState({
+                    data: res.data
+                })
+            })
+    }
+
+    componentDidMount() {
+        this.getProducts();
+    }
+
+
+    render() {
+        return (
+            <>
+                <Sidebar />
+                <div className="main-content">
+                    <DashNav />
+                    <main>
+                        <h2 className="dash-title">Product-List</h2>
+                        <Link to="/dashboard/addProduct"><button className="primary">Add Product</button></Link>
+                        <div className="list">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Product Name</th>
+                                        <th>Product Image</th>
+                                        <th>Description</th>
+                                        <th>Category</th>
+                                        <th>Price</th>
+                                        <th>Quantity</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.data.map((result, index) => {
+                                            return (
+                                                <tr key={index}>
+                                                    <td>{result.pname}</td>
+                                                    <td><img src={`../images/${result.iname}`} alt="cactus" /></td>
+                                                    <td>{result.p_description}</td>
+                                                    <td>{result.cname}</td>
+                                                    <td>{result.price}</td>
+                                                    <td>{result.quantity}</td>
+                                                    <td>
+                                                        <button className="edit"><i className="fas fa-pencil-alt"> Edit</i></button>
+                                                        <button className="delete"><i className="fas fa-trash-alt"> Delete</i></button>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </main>
+                </div>
+            </>
+        )
+    }
 }
