d5beb6efd9b429a80c60772d48091b9454b3abd4
ajitaz <azalbert.ab@gmail.com>
2021-03-16 22:31:45 +0545

    pre EditNursery

---
+++ src/components/Dashboard/Functions/EditNursery/editNursery.component.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './editNursery.component.css';
+
+const defaultForm = {
+
+}
+
+export class EditNursery extends Component {
+
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+    componentDidMount() {
+
+    }
+
+
+    handleChange = () => {
+
+    }
+
+    handleSubmit = () => {
+
+    }
+
+    render() {
+        return (
+            <>
+
+            </>
+        )
+    }
+}
