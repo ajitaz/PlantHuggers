09c95b565e3d502c9d20544db5f032c9c269a72f
ajitaz <azalbert.ab@gmail.com>
2021-03-20 22:44:49 +0545

    Dynamic shop

--- src/components/Pages/Shop/shop.component.js
+++ src/components/Pages/Shop/shop.component.js
@@ -1,73 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Footer } from '../../common/footer/footer.component';
 import { NavBar } from '../../common/navbar/nav.component';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './shop.component.css';
+import axios from 'axios';
 
-export const Shop = () => {
-    return (
-        <>
-            <NavBar />
-            <div className="small-container">
-            <h2 className="title">All Products</h2>
-                <div className="product-list">
-                    <h2>Indoor Plants</h2>
-                    <div className="grid">
-                        <div className="col-4">
-                            <Link to="/product/cactus"><img src="../images/plant2.jpg" alt="" /></Link>
-                            <h4>Plant Indoor</h4>
-                            <p>Rs.1800</p>
-                        </div>
-                        <div className="col-4">
-                            <img src="../images/plant1.jpg" alt="" />
-                            <h4>Plant Indoor</h4>
-                            <p>Rs.1800</p>
-                        </div>
-                        <div className="col-4">
-                            <img src="../images/plant2.jpg" alt="" />
-                            <h4>Plant Indoor</h4>
-                            <p>Rs.1800</p>
-                        </div>
-                        <div className="col-4">
-                            <img src="../images/plant1.jpg" alt="" />
-                            <h4>Plant Indoor</h4>
-                            <p>Rs.1800</p>
-                        </div>
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-                    </div>
-                    <button><Link to="/category/indoor">view more</Link></button>
-                    <div className="clear"></div>
+export class Shop extends Component {
+    constructor() {
+        super();
+        this.state = {
+            categories: [],
+            products: []
+        }
+    }
+
+    getCategories() {
+        axios.get(`${BASE_URL}/viewContent.php?option=category`)
+            .then(res => {
+                this.setState({
+                    categories: res.data
+                })
+            })
+    }
+
+    getProducts() {
+        axios.get(`${BASE_URL}/viewContent.php?option=viewProduct`)
+            .then(res => {
+                this.setState({
+                    products: res.data
+                })
+            })
+    }
+
+    componentDidMount() {
+        this.getCategories();
+        this.getProducts();
+    }
+
+
+    render() {
+        return (
+            <>
+                <NavBar />
+                <div className="small-container">
+                    <h2 className="title">All Products</h2>
+                    {
+                        this.state.categories.map((result, index) => {
+                            return (
+                                <div key={index} className="product-list">
+                                    <h2>{result.cname}</h2>
+                                    <div className="grid">
+                                        {
+                                            this.state.products.map((product, indx) => {
+                                                if (product.cid == result.cid) {
+                                                    return (
+                                                        <div className="col-4">
+                                                            <Link to={`/product/${product.pname}`}><img src={`../images/${product.iname}`} alt="" /></Link>
+                                                            <h4>{product.pname}</h4>
+                                                            <p>{product.price}</p>
+                                                        </div>
+                                                    )
+                                                }
+
+                                            })
+                                        }
+
+                                    </div>
+                                    <button><Link to={`/category/${result.cname}`}>view more</Link></button>
+                                    <div className="clear"></div>
+                                </div>
+                            )
+                        })
+
+                    }
+
                 </div>
-
-                <div className="product-list">
-                    <h2>Outdoor Plant</h2>
-                    <div className="grid">
-                        <div className="col-4">
-                            <img src="../images/plant1.jpg" alt="" />
-                            <h4>Plant Indoor</h4>
-                            <p>Rs.1800</p>
-                        </div>
-                        <div className="col-4">
-                            <img src="../images/plant2.jpg" alt="" />
-                            <h4>Plant Indoor</h4>
-                            <p>Rs.1800</p>
-                        </div>
-                        <div className="col-4">
-                            <img src="../images/plant1.jpg" alt="" />
-                            <h4>Plant Indoor</h4>
-                            <p>Rs.1800</p>
-                        </div>
-                        <div className="col-4">
-                            <img src="../images/plant1.jpg" alt="" />
-                            <h4>Plant Indoor</h4>
-                            <p>Rs.1800</p>
-                        </div>
-                    </div>
-                    <button><a href="#">view more</a></button>
-                    <div className="clear"></div>
-                </div>
-            </div>
-            <Footer />
-        </>
-    )
+                <Footer />
+            </>
+        )
+    }
 }
