8626d6256e1f744bd01f6d00943d753b63683893
chudaraj poudyal <crpoudyal@gmail.com>
2021-03-27 10:24:41 +0545

    banner image text fixed

--- src/components/Section/Header/header.component.js
+++ src/components/Section/Header/header.component.js
@@ -4,7 +4,7 @@ import { Banner } from '../Banner/banner.component';
 
 export const Header = () => {
     return(
-        <div className="nav-container" style ={{backgroundImage: "url(/images/garden-party-chairs-s2.jpg)"}} >
+        <div className="nav-container" style ={{backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)),url(/images/garden-party-chairs-s2.jpg)"}} >
             <Banner/>
         </div>
     )
