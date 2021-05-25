0a6262ee9041356a38e9d5e8769b1f19c750570c
ajitaz <azalbert.ab@gmail.com>
2021-05-25 15:26:32 +0545

    Screen Rize function

---
+++ src/components/common/ScreenResize/screenResize.component.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function debounce(fn, ms) {
+    let timer
+    return _ => {
+        clearTimeout(timer)
+        timer = setTimeout(_ => {
+            timer = null
+            fn.apply(this, arguments)
+        }, ms)
+    };
+}
+
+export default function ScreenSize() {
+    const [dimensions, setDimensions] = React.useState({
+        width: window.innerWidth
+    })
+    React.useEffect(() => {
+        const debouncedHandleResize = debounce(function handleResize() {
+            setDimensions({
+                width: window.innerWidth
+            })
+        }, 500)
+
+        window.addEventListener('resize', debouncedHandleResize)
+
+        return _ => {
+            window.removeEventListener('resize', debouncedHandleResize)
+
+        }
+    })
+    return dimensions.width
+}
+
+
